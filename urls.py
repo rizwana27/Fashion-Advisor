e0448defbc84cac3d56642245e3e48d42576e370
Shaik Rizwana <85737939+rizwana27@users.noreply.github.com>
2023-10-27 18:59:11 -0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,45 +1,23 @@
 """
-Definition of urls for DjangoWebProject_fashion_design.
+URL configuration for fashion_advisor project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from datetime import datetime
-from django.urls import include, path
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
-from app import forms, views
-
-
-
-
-
-
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
-    path('about/', views.about, name='about'),
-    
-    
-    
-    #custom
-    path('text_entry_view/',views.text_entry_view, name='text_entry_view'),
-    
-
-    
-    
-    path('login/',
-         LoginView.as_view
-         (
-             template_name='app/login.html',
-             authentication_form=forms.BootstrapAuthenticationForm,
-             extra_context=
-             {
-                 'title': 'Log in',
-                 'year' : datetime.now().year,
-             }
-         ),
-         name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('admin/', admin.site.urls),
-   
+    path("admin/", admin.site.urls),
+    path('',include('authentication.urls')),
 ]
