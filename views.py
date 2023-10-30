aef80a295b21bf33eda90eaeef4605212af651a6
Pavan BT <8897pavan@gmail.com>
2023-10-30 15:21:22 -0500

    updated py files for integration

--- views.py
+++ views.py
@@ -3,8 +3,16 @@ Definition of views.
 """
 
 from datetime import datetime
-from django.shortcuts import render
 from django.http import HttpRequest
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import CustomUserCreationForm
+
+from django.contrib.auth import authenticate, login
+
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 def home(request):
     """Renders the home page."""
@@ -18,5 +26,39 @@ def bodyshape_view(request):
 
 def skintype_view(request):
     return render(request, 'app/skintype.html')
+
+
+def signup_view(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('home')  # redirect to login page
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'app/signup.html', {'form': form})
+
+def signin(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Welcome, {username}!')
+                return redirect('home')  # or wherever you want to redirect after login
+            else:
+                messages.error(request, 'Invalid username or password.')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'app/signin.html', {'form': form})
+
+
 
     
